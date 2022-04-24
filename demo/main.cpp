53e0958ab365418986819c2d9378b588b7fcd31c
Мария Мельникова <mariamelnikova@MacBook-Pro.local>
2022-04-24 19:17:59 +0300

    test

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,11 @@
-#include <example.hpp>
-
+// Copyright 2022 Melnikova Maria <dandelion.m@mail.ru>
+#include "client.hpp"
 int main() {
-  example();
+  Client cl;
+  // {"input":"hel"}
+  cl.request();
+  std::cout << std::endl;
+  std::cout << "RESPONSE: " << std::endl;
+  std::cout << std::endl;
+  cl.start();
 }
